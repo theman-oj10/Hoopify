dbedc6431e2f0d76f69bc4e3def484c33f79fbb9
Manoj <manojnarender@gmail.com>
2023-05-29 01:08:18 +0800

    Main merge

---
+++ frontend/Screens/HomePage/HomePage.js
@@ -0,0 +1,194 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import { auth, storage, db } from '../../firebase';
+import { useNavigation } from '@react-navigation/native';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { doc, setDoc } from 'firebase/firestore';
+
+import Logo from '../SignInScreen/Images/Logo.png';
+
+const HomePage = () => {
+  const navigation = useNavigation();
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const onFileChange = (files) => {
+    const currentFile = files[0];
+    console.log(currentFile);
+  };
+
+  const uploadToDatabase = (url) => {
+    const docData = {
+      mostRecentUploadURL: url,
+      username: auth.currentUser?.email,
+    };
+
+    const userRef = doc(db, 'users', docData.username);
+    setDoc(userRef, docData, { merge: true })
+      .then(() => {
+        console.log('Successfully updated DB');
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  };
+
+  const handleClick = async () => {
+    try {
+      let filePickerOptions = {
+        type: '*/*',
+        copyToCacheDirectory: true,
+      };
+
+      if (Platform.OS !== 'web') {
+        filePickerOptions = {
+          ...filePickerOptions,
+          multiple: false,
+        };
+      }
+
+      const result = await DocumentPicker.getDocumentAsync(filePickerOptions);
+
+      if (result.type === 'success') {
+        const { name, uri } = result;
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        const userId = auth.currentUser?.uid;
+        const emailId = auth.currentUser?.email;
+        const filename = `${emailId}/${userId}/${name}`;
+
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, blob);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setUploadProgress(progress);
+          },
+          (error) => {
+            console.log('File upload error:', error);
+            setUploadProgress(0);
+          },
+          async () => {
+            const downloadUrl = await getDownloadURL(uploadTask.snapshot.ref);
+            console.log('File uploaded successfully. Download URL:', downloadUrl);
+            uploadToDatabase(downloadUrl);
+            setUploadProgress(0);
+          }
+        );
+      }
+    } catch (error) {
+      console.log('Error selecting file:', error);
+    }
+  };
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace('Sign-In');
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <View style={styles.root}>
+      <View style={styles.container}>
+        <Image source={Logo} style={styles.logo} resizeMode="contain" />
+        <Text style={styles.title}>Hoopify</Text>
+        <Text style={styles.email}>Email: {auth.currentUser?.email}</Text>
+        
+        <TouchableOpacity style={styles.uploadButton} onPress={handleClick}>
+          <View style={styles.uploadButtonInner}>
+            <Text style={styles.buttonText}>Upload File</Text>
+          </View>
+        </TouchableOpacity>
+        
+        {uploadProgress > 0 && (
+          <Text style={styles.uploadProgress}>Upload Progress: {Math.round(uploadProgress)}%</Text>
+        )}
+
+        <TouchableOpacity style={styles.signOutButton} onPress={handleSignOut}>
+          <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.statsButton}
+          onPress={() => navigation.navigate('StatsPage')}
+        >
+          <Text style={styles.buttonText}>Show My Stats</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'pink',
+  },
+  container: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: '70%',
+    height: 100,
+    borderRadius: 10,
+    marginBottom: 40,
+  },
+  title: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#333',
+  },
+  email: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: '#777',
+  },
+  uploadButton: {
+    backgroundColor: '#4287f5',
+    width: '100%',
+    borderRadius: 10,
+    marginTop: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  uploadProgress: {
+    marginTop: 10,
+    fontSize: 16,
+    color: '#777',
+  },
+  signOutButton: {
+    backgroundColor: '#ff4136',
+    width: '100%',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  statsButton: {
+    backgroundColor: '#90ee90',
+    width: '100%',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
+
+export default HomePage;
---
+++ frontend/Screens/HotZonePage/HotZonePage.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import HotZone from '../SignInScreen/Images/HotZone.png'
+import { useNavigation } from '@react-navigation/native';
+
+const HotZonePage = () => {
+  // Sample data
+  const hotZones = ['Left Corner 3', 'Right Wing', 'Paint'];
+  const weakZones = ['Top of the Key', 'Left Elbow'];
+  
+  const handleShare = () => {
+    // Handle the action for sharing
+    console.log('Share');
+  };
+
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hotzones</Text>
+      <Image source={HotZone} style={styles.logo} resizeMode="contain" />
+      
+      <Text style={styles.title}>Hot Zones</Text>
+      <View style={styles.hotZoneContainer}>
+        {hotZones.map((zone, index) => (
+          <Text key={index} style={styles.hotZone}>{zone}</Text>
+        ))}
+      </View>
+      
+      <Text style={styles.title}>Weak Zones</Text>
+      <View style={styles.weakZoneContainer}>
+        {weakZones.map((zone, index) => (
+          <Text key={index} style={styles.weakZone}>{zone}</Text>
+        ))}
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('StatsPage')}>
+            <Text style={styles.buttonText}>Back to my Stats</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleShare}>
+          <Text style={styles.buttonText}>Share</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'pink',
+  },
+  title: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 400,
+    height: 300,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  hotZoneContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  hotZone: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    margin: 5,
+    backgroundColor: 'green',
+    color: 'white',
+    borderRadius: 5,
+  },
+  weakZoneContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  weakZone: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    margin: 5,
+    backgroundColor: 'red',
+    color: 'white',
+    borderRadius: 5,
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: '#4287f5',
+    width: '100%',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default HotZonePage;
