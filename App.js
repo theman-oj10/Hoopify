e26b13b18e4998667abf795b17ecb6e0101c5964
AravinthAdarsh <111167525+AravinthAdarsh@users.noreply.github.com>
2023-05-28 20:38:31 +0800

    new

---
+++ App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './Screens/SignInScreen';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomePage from './Screens/HomePage/HomePage';
+import { auth } from './firebase';
+import StatsPage from './Screens/StatsPage/StatsPage';
+import ReportPage from './Screens/ReportPage/ReportPage';
+import HotZonePage from './Screens/HotZonePage/HotZonePage';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Sign-In" component={SignInScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="HomePage" component={HomePage} />
+        <Stack.Screen options={{ headerShown: false }} name="StatsPage" component={StatsPage} />
+        <Stack.Screen options={{ headerShown: false }} name="ReportPage" component={ReportPage} />
+        <Stack.Screen options={{ headerShown: false }} name="HotZonePage" component={HotZonePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+});
---
+++ firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHLyLBe7Bh5Q48rUK2-x8-A6A2vxk0hdI",
+  authDomain: "orbital-app-proto.firebaseapp.com",
+  projectId: "orbital-app-proto",
+  storageBucket: "orbital-app-proto.appspot.com",
+  messagingSenderId: "965591983424",
+  appId: "1:965591983424:web:759b1b999d60cfd6e6c6a5",
+  measurementId: "G-JV5TKFE1BX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { auth, storage, db };
+
+
+
+
+
