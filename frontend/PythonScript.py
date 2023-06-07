305a155247839c6a952a0b31f3b01d632050c5d1
AravinthAdarsh <111167525+AravinthAdarsh@users.noreply.github.com>
2023-06-08 01:07:31 +0800

    newww

--- frontend/PythonScript.py
+++ frontend/PythonScript.py
@@ -8,61 +8,50 @@ from cvzone.ColorModule import ColorFinder
 import math
 from sort import *
 import numpy as np
-import pyrebase
-from ShotTracker import YoloBallTracker
+from flask_cors import CORS
+from firebase_admin import credentials, initialize_app, storage, auth
+import os
+from ShotTracker.YoloBallTracker import yoloTrack
+
 
 
 app = Flask(__name__)
 load_dotenv()
+CORS(app)
 
-firebaseConfig = {
-  'apiKey': "AIzaSyCHLyLBe7Bh5Q48rUK2-x8-A6A2vxk0hdI",
-  'authDomain': "orbital-app-proto.firebaseapp.com",
-  'projectId': "orbital-app-proto",
-  'storageBucket': "orbital-app-proto.appspot.com",
-  'messagingSenderId': "965591983424",
-  'appId': "1:965591983424:web:759b1b999d60cfd6e6c6a5",
-  'measurementId': "G-JV5TKFE1BX"
-}
+cred = credentials.Certificate('C:/Users/Adarsh/Desktop/hoopify/Hoopify-master/orbital-app-proto-firebase-adminsdk-4agc7-9fd8bd28f7.json')  # Path to your service account key file
+initialize_app(cred, {
+    'storageBucket': "orbital-app-proto.appspot.com"  
+})
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-storage = firebase.storage()
 
 model = YOLO("../Yoloweights/yolov8n.pt")
 
 classNames = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
-limits = [580, 160, 650, 160]
-totalCount = 0
-tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
-detections = np.empty((0, 5))
-basketCounted = False
-previousDetections = []
-
 def perform_video_analysis(video_path):
-    return YoloBallTracker.yoloTrack(video_path)
+    return yoloTrack(video_path)
 
-@app.route('/api/video-analysis', methods=['POST'])
+
+@app.route('/api/video-analysis', methods=['GET'])
 def video_analysis():
-    # Check if the request contains a file
-    if 'video' not in request.files:
-        return jsonify({'error': 'No video file found'})
+    try:
+        # Retrieve the video file from Firebase Storage
+        bucket = storage.bucket()
+        blob = bucket.blob('hello@gmail.com/1HNMtHwFxYSPVc1FFMNX9bX4W7h1/Video3.mp4')
+        temp_video_path = 'C:/Users/Adarsh/Desktop/hoopify/Hoopify-master/Videos/temp_video.mp4'  # Replace with the desired path for the temporary video file
+        blob.download_to_filename(temp_video_path)
 
-    video_file = request.files['video']
+        # Analyze the video
+        score = perform_video_analysis(temp_video_path)
 
-    # Save the video file
-    filename = secure_filename(video_file.filename)
-    video_path = f'uploads/{filename}'
-    video_file.save(video_path)
+        # Delete the temporary video file
+        os.remove(temp_video_path)
 
-    # Perform video analysis using your Python code
-    score = perform_video_analysis(video_path)
-
-    # Delete the local video file after analysis
-    os.remove(video_path)
-
-    # Return the analysis score
-    return jsonify({'score': score})
+        # Return the score as a response
+        return f"Video analysis score: {score}"
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
