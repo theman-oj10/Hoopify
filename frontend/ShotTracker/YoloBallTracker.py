75b020469819b017b673c1fe3592961833ae0998
Manoj <manojnarender@gmail.com>
2023-06-23 22:14:10 +0800

    added position tracking feature in backend

--- frontend/ShotTracker/YoloBallTracker.py
+++ frontend/ShotTracker/YoloBallTracker.py
@@ -1,40 +1,99 @@
 import cv2
 from ultralytics import YOLO
-from . import FindRim
+import numpy as np
+import FindRim
+import PositionTracker as pt
 
-def intersect(ballX, ballY, x1, x2, y1, y2, x3, y3, x4, y4):
+def intersect(ballX, ballY, x1, y1, x2, y2, x3, y3, x4, y4):
     # equation of rim line
     m = (y2 - y1)/(x2 - x1)
     c = y2 - (m * x2)
 
+    minX = min(x1, x2, x3, x4)
+    maxX = max(x1, x2, x3, x4)
+    minY = min(y1, y2, y3, y4)
+    maxY = max(y1, y2, y3, y4)
     # check if ball intersects
-    return ballY == m*ballX + c
+    #return ballY == m*ballX + c
+    return minX <= ballX <= maxX and minY <= ballY <= maxY
 def yoloTrack(path):
     model = YOLO('yolov8l.pt')
     # Filtering the classes: !Doesn't work still tracks everything
     model.classes = ['person', 'sports ball']
 
     # recording the rim-cordinates
-    #rim_coordinates = FindRim.find_rim(path)
-    rim_coordinates = [(577,91), (667, 91), (574, 207), (662, 211)]
+    rim_coordinates = FindRim.find_rim(path)
     # checking shot attempts
     # in possesion => overlap between ball and person bbox
 
     #cv2.destroyAllWindows()
     print(f"rim coordinates: {rim_coordinates}")
-    results = model.predict(source=path, show=True, device='mps', stream='True')
+    results = model.predict(source=path, show=True, stream='True')
+    shots_taken = 0
     crossed_rim = False
-    crossed_net = False
     score = 0
     ball_coordinates = []
+    def check_overlap(ball_bbox, ball_x, ball_y, person_bbox, person_x, person_y):
+        ball_w = ball_bbox[2]
+        ball_h = ball_bbox[3]
+        person_w = person_bbox[2]
+        person_h = person_bbox[3]
+        ball_rightTop = (ball_x + ball_w/2, ball_y + ball_h/2)
+        ball_leftTop = (ball_x - ball_w/2, ball_y + ball_h/2)
+        ball_rightBottom = (ball_x + ball_w / 2, ball_y - ball_h / 2)
+        ball_leftBottom = (ball_x - ball_w / 2, ball_y - ball_h / 2)
+        person_rightTop = (person_x + person_w / 2, person_y + person_h / 2)
+        person_leftTop = (person_x - person_w / 2, person_y + person_h / 2)
+        person_rightBottom = (person_x + person_w / 2, person_y - person_h / 2)
+        person_leftBottom =(person_x - person_w / 2, person_y - person_h / 2)
+
+        return intersect(ball_x, ball_y, person_leftTop[0], person_leftTop[1], person_rightTop[0], person_rightTop[1], person_leftBottom[0],
+                         person_leftBottom[1], person_rightBottom[0], person_rightBottom[1])
+    person_exists = False
+    shot_tobeTaken = False
+    # finding homography matrix:
+    matrix = pt.find_homography_matrix(path, "TestFootage/court_invert.png")
+    # each result is a frame
     for result in results:
         boxes = result.boxes
         frame = 0
+
+        # boxes contains all the bboxes in a frame
+        # if a frame contains both a person and a ball
+        # check if frame and ball overlap
         for box in boxes:
+            if int(box.cls) == 0: # '0' is the class index for 'person'
+                personbBox = box.xywh[0]
+                x_person = personbBox[0]
+                y_person = personbBox[1]
+                # player position tracking
+                # apply homography transformation
+                original_coord = np.array([[x_person, y_person, 1]])
+                transformed_coord = np.dot(matrix, original_coord.T)
+                # Normalize the transformed coordinate
+                normalized_coord = transformed_coord / transformed_coord[2]
+                # Extract the x and y values of the normalized coordinate
+                transformed_x = normalized_coord[0, 0]
+                transformed_y = normalized_coord[1, 0]
+                person_exists = True
             if int(box.cls) == 32:  # '32' is the class index for 'ball'
                 currbBox = box.xywh[0]
                 x_center = currbBox[0]
                 y_center = currbBox[1]
+                # checking for # shots taken
+                # check if bbox of person and ball overlap => shots to be taken
+                if person_exists:
+                    if check_overlap(currbBox, x_center, y_center, personbBox, x_person, y_person):
+                        print('Shot to be Taken')
+                        shot_tobeTaken = True
+                        # bbox of person and ball no longer overlap => shot taken
+                if shot_tobeTaken and not check_overlap(currbBox, x_center, y_center, personbBox, x_person, y_person):
+                    shot_tobeTaken = False
+                    print("Shot Taken")
+                    shots_taken += 1
+                    print(f"x-player: {transformed_x}")
+                    print(f"y-player: {transformed_y}")
+                    person_exists = False
                 ball_coordinates.append((x_center, y_center))
                 #print(ball_coordinates)
                 # checking if it crosses the rim
@@ -57,10 +116,11 @@ def yoloTrack(path):
                         crossed_rim = False
                         frame = 0
                 frame += 1
-                # (x,y) of left end of rim, (x,y) of right of rim, (x,y) of left of net, (x,y) of right of net
+                    # (x,y) of left end of rim, (x,y) of right of rim, (x,y) of left of net, (x,y) of right of net
 
     # returning the co-ordinates of the center of the ball
     print(f"Score: {score}")
+    print(f"Shots: {shots_taken}")
     # print(results)
     return score
 
@@ -69,5 +129,5 @@ def yoloTrack(path):
 # first find the co-ordinates of the middle of the net
 # checking if it crosses the rim
 
-#yoloTrack("TestFootage/shotclip.mp4")
+yoloTrack("TestFootage/MAIN_TEST.mov")
 
