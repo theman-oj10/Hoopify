e02591cd7c063e1a0879ee0118a5b19d0cb8840e
AravinthAdarsh <111167525+AravinthAdarsh@users.noreply.github.com>
2023-06-24 17:05:25 +0800

    ms2 work

--- frontend/PythonScript.py
+++ frontend/PythonScript.py
@@ -9,9 +9,10 @@ import math
 from sort import *
 import numpy as np
 from flask_cors import CORS
+import firebase_admin
 from firebase_admin import credentials, initialize_app, storage, auth, firestore
 import os
-from ShotTracker.YoloBallTracker import yoloTrack
+# from ShotTracker.YoloBallTracker import yoloTrack
 
 
 
@@ -29,44 +30,40 @@ model = YOLO("../Yoloweights/yolov8n.pt")
 
 classNames = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
+'''
 def perform_video_analysis(video_path):
     return yoloTrack(video_path)
+
+'''
 
 
 @app.route('/api/video-analysis', methods=['GET'])
 def video_analysis():
     try:
         # Retrieve the video file from Firebase Storage
-        # Get the currently signed-in user's email
-        user_email = auth.current_user().email
-        
         bucket = storage.bucket()
-        blob = bucket.blob('${user_email}/1HNMtHwFxYSPVc1FFMNX9bX4W7h1/Video3.mp4')
+        blob = bucket.blob('hello@gmail.com/1HNMtHwFxYSPVc1FFMNX9bX4W7h1/Video3.mp4')
         temp_video_path = 'C:/Users/Adarsh/Desktop/hoopify/Hoopify-master/Videos/temp_video.mp4'  # Replace with the desired path for the temporary video file
         blob.download_to_filename(temp_video_path)
 
         # Analyze the video
-        score = perform_video_analysis(temp_video_path)
+        # score = perform_video_analysis(temp_video_path)
+        score = 70
+        # totalShots = 100
         
         db = firestore.client()
-
-        # Retrieve the user based on the email
-        user = auth.get_user_by_email(user_email)
-
-        # Retrieve the userID
-        userID = user.uid
         
         # Create a document reference with the userID as the document ID
-        doc_ref = db.collection('scores').document(userID)
+        # doc_ref = db.collection('scores').document()
 
         # Set the score value in the document
-        doc_ref.set({'score': score})
+        # doc_ref.set({'score': score})
 
         # Delete the temporary video file
         os.remove(temp_video_path)
 
         # Return the score as a response
-        return f"Video analysis score: {score}"
+        return f"{score}"
     except Exception as e:
         return str(e)
 
